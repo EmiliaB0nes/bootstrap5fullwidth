f2cc993438a8ab2f8cd9bcc7a3a612e05fb69b72
Ludestru <Ludestru@users.noreply.github.com>
2021-06-17 15:52:21 +0200

    Add condition for CarbonField-updated translation

--- function-carbon_fields.php
+++ function-carbon_fields.php
@@ -22,16 +22,21 @@ function crb_front_page()
     Container::make('post_meta', esc_html__('Front Page Options', 'wpb5Translations'))
         ->where('post_id', '=', get_option('page_on_front'))
         ->add_fields(array(
-            Field::make('text', 'crb_front_main_title', esc_html__('Main Title', 'wpb5Translations')),
-            Field::make('text', 'crb_subtitle', esc_html__('Subtitle', 'wpb5Translations')),
-            Field::make('checkbox', 'crb_show_skills', esc_html__('Show skills', 'wpb5Translations')),
+            Field::make('text', 'crb_front_main_title', esc_html__('Main Title', 'wpb5Translations'))
+            ->set_default_value( esc_html__('My Title', 'wpb5Translations'))
+            ->set_required( true ),
+            Field::make('text', 'crb_subtitle', esc_html__('Subtitle', 'wpb5Translations'))
+            ->set_default_value( esc_html__('My Subtitle', 'wpb5Translations')),
+            Field::make('checkbox', 'crb_show_skills', esc_html__('Show skills', 'wpb5Translations'))
+            ->set_default_value( true ),
             Field::make('text', 'crb_skills_area_title', esc_html__('Skills area title', 'wpb5Translations'))
                 ->set_conditional_logic(array(
                     array(
                         'field' => 'crb_show_skills',
                         'value' => true,
                     )
-                )),
+                ))
+                ->set_required( true ),
             Field::make('complex', 'crb_skill_slider', esc_html__('Skills List', 'wpb5Translations'))
                 ->set_conditional_logic(array(
                     array(
@@ -40,8 +45,10 @@ function crb_front_page()
                     )
                 ))
                 ->add_fields(array(
-                    Field::make('text', 'skill_title', esc_html__('Skill', 'wpb5Translations')),
-                    Field::make('image', 'skill_logo', esc_html__('Skill Logo', 'wpb5Translations')),
+                    Field::make('text', 'skill_title', esc_html__('Skill', 'wpb5Translations'))
+                    ->set_required( true ),
+                    Field::make('image', 'skill_logo', esc_html__('Skill Logo', 'wpb5Translations'))
+                    ->set_required( true ),
                 )),
 
 
@@ -59,17 +66,25 @@ function crb_competence_page()
             return $slug === 'competences';
         } )
         ->add_fields(array(
-            Field::make('text', 'crb_competence_slider1_title', esc_html__('List 1 Title', 'wpb5Translations')),
+            Field::make('text', 'crb_competence_slider1_title', esc_html__('List 1 Title', 'wpb5Translations'))
+            ->set_default_value( esc_html__('My Title', 'wpb5Translations'))
+            ->set_required( true ),
             Field::make('complex', 'crb_competence_slider1', esc_html__('Competence List 1', 'wpb5Translations'))
                 ->add_fields(array(
-                    Field::make('text', 'skill_title', esc_html__('Competence title', 'wpb5Translations')),
-                    Field::make('image', 'skill_logo', esc_html__('Competence Logo', 'wpb5Translations')),
+                    Field::make('text', 'skill_title', esc_html__('Competence title', 'wpb5Translations'))
+                    ->set_required( true ),
+                    Field::make('image', 'skill_logo', esc_html__('Competence Logo', 'wpb5Translations'))
+                    ->set_required( true ),
                 )),
-            Field::make('text', 'crb_competence_slider2_title', esc_html__('List 2 Title', 'wpb5Translations')),
+            Field::make('text', 'crb_competence_slider2_title', esc_html__('List 2 Title', 'wpb5Translations'))
+            ->set_default_value( esc_html__('My Title', 'wpb5Translations'))
+            ->set_required( true ),
             Field::make('complex', 'crb_competence_slider2', esc_html__('Competence List 2', 'wpb5Translations'))
                 ->add_fields(array(
-                    Field::make('text', 'skill_title', esc_html__('Competence title', 'wpb5Translations')),
-                    Field::make('image', 'skill_logo', esc_html__('Competence Logo', 'wpb5Translations')),
+                    Field::make('text', 'skill_title', esc_html__('Competence title', 'wpb5Translations'))
+                    ->set_required( true ),
+                    Field::make('image', 'skill_logo', esc_html__('Competence Logo', 'wpb5Translations'))
+                    ->set_required( true ),
                 )),
 
 
